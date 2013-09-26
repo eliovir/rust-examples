ab0ae195780d97347586b704d3ec24a905cd3791
eliovir <eliovir@gmail.com>
2013-09-27 01:09:40 +0200

    Update tutorial-02_1-hello.rs

--- tutorial-02_1-hello.rs
+++ tutorial-02_1-hello.rs
@@ -1,6 +1,6 @@
 /**
  * 2.1 Compiling your first program
- * http://static.rust-lang.org/doc/0.7/tutorial.html#getting-started
+ * http://static.rust-lang.org/doc/0.7/tutorial.html#compiling-your-first-program
  *
  * Run `rustc hello.rs` to produce an executable called `hello` (or `hello.exe` on Windows).
  * Run `rust run hello.rs` to compile and run this code.
