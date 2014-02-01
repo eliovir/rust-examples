3de55c37adef920356d1d3622326bfdb9c297be8
Eliovir <eliovir@gmail.com>
2014-02-01 09:06:22 +0100

    Add new code

---
+++ tutorial-04_2-pattern-matching.rs
@@ -0,0 +1,59 @@
+/**
+ * 4.2 Pattern matching
+ * http://static.rust-lang.org/doc/master/tutorial.html#pattern-matching
+ *
+ * @license MIT license <http://www.opensource.org/licenses/mit-license.php>
+ */
+use std::rand::{task_rng, Rng};
+use std::f64;
+use std::num::atan;
+fn angle(vector: (f64, f64)) -> f64 {
+	let pi = f64::consts::PI;
+	/*
+	 * A powerful application of pattern matching is destructuring:
+	 * matching in order to bind names to the contents of data types.
+	 */
+	match vector {
+		(0.0, y) if y < 0.0 => 1.5 * pi,
+		(0.0, _) => 0.5 * pi,
+		(x, y) => atan(y / x)
+	}
+}
+fn main() {
+	let mut rng = task_rng();
+	let my_number: uint = rng.gen_range(0u, 10);
+	println!("my_number = {}", my_number);
+
+	/*
+	 * Rust's match construct is a generalized, cleaned-up version of C's
+	 * switch construct. You provide it with a value and a number of arms,
+	 * each labelled with a pattern, and the code compares the value
+	 * against each pattern in order until one matches. The matching
+	 * pattern executes its corresponding arm.
+	 */
+	match my_number {
+		0     => println!("zero"),
+		1 | 2 => println!("one or two"),
+		3..10 => println!("three to ten"),
+		_     => println!("something else")
+	}
+	/*
+	 * match constructs must be exhaustive: they must have an arm covering
+	 * every possible case. For example, the typechecker would reject the
+	 * previous example if the arm with the wildcard pattern was omitted.
+	 */
+	match my_number {
+		0 => { println!("zero") }
+		_ => { println!("something else") }
+	}
+
+	let age: uint = rng.gen_range(0u, 100);
+	println!("age = {}", age);
+	match age {
+		a @ 0..20 => println!("{} years old", a),
+		_ => println!("older than 21")
+	}
+
+	let vector = (1f64,1f64);
+	println!("angle({:?}) == {:?}", vector, angle(vector));
+}
---
+++ tutorial-05_1-structs.rs
@@ -0,0 +1,36 @@
+/**
+ * 5.1 Structs
+ * http://static.rust-lang.org/doc/master/tutorial.html#structs
+ *
+ * @license MIT license <http://www.opensource.org/licenses/mit-license.php>
+ */
+/*
+ * Rust struct types must be declared before they are used using the struct syntax.
+ */
+struct Point {
+	x: f64,
+	y: f64
+}
+fn main() {
+	let mut mypoint = Point { x: 1.0, y: 1.0 };
+	let origin = Point { x: 0.0, y: 0.0 };
+
+	mypoint.y += 1.0; // mypoint is mutable, and its fields as well
+	//origin.y += 1.0; // ERROR: assigning to immutable field
+	
+	// `match` patterns destructure structs.
+	match mypoint {
+		Point { x: 0.0, y: yy } => println!("{}", yy),
+		Point { x: xx,  y: yy } => println!("{} {}", xx, yy),
+	}
+
+	// When you are not interested in all the fields of a struct,
+	// a struct pattern may end with `, _` 
+	// (as in `Name { field1, _ }`) to indicate that you're
+	// ignoring all other fields.
+	// Additionally, struct fields have a shorthand matching form 
+	// that simply reuses the field name as the binding name.
+	match mypoint {
+		Point { x, .. } => println!("{}", x),
+	}
+}
