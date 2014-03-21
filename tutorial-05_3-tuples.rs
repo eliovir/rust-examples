1d32c216960ab9cb43e82d335f8f03af72b182c8
Eliovir <eliovir@gmail.com>
2014-03-21 16:41:23 +0100

    Changes to version 0.10-pre (af93684 2014-03-17 11:57:08)

--- tutorial-05_3-tuples.rs
+++ tutorial-05_3-tuples.rs
@@ -12,7 +12,7 @@ fn main() {
 
 	let mytup: (int, int, f64) = (10, 20, 30.0);
 	match mytup {
-		(a, b, c) => info!("{}", a + b + (c as int))
+		(a, b, c) => println!("{}", a + b + (c as int))
 	}
 
 	let tuple1 = (5i, 6i);
