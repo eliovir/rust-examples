4b577ba86f544afc42db5a7a2c13a23f55f06052
Eliovir <eliovir@gmail.com>
2014-04-21 23:11:47 +0200

    Correct design_pattern-observer.rs

--- design_pattern-observer.rs
+++ design_pattern-observer.rs
@@ -1,26 +1,40 @@
+#![crate_id="design_pattern-observer#1.0"]
+#![crate_type = "bin"]
+#![license = "MIT"]
+#![desc = "Example of design pattern inspired from Head First Design Patterns"]
+//! Example of design pattern inspired from Head First Design Patterns
 //!
+//! Tested with rust-0.11-pre
+//!
+//! @author Eliovir <http://github.com/~eliovir>
+//!
+//! @license MIT license <http://www.opensource.org/licenses/mit-license.php>
+//!
+//! @since 2014-04-18
+//! TODO : add/delete/notify observers of different types
+//! TODO : display Observable temperature on Observer.update()
 
 // Observer
-//trait Observer<U: Observable> {
 trait Observer {
-	fn update(&mut self);
+	fn update(&self);
 }
 
 // Observable memorizes all Observers and send notifications
-trait Observable<T: Observer> {
-	fn addObserver(&mut self, observer: ~T);
-	fn deleteObserver(&mut self, observer: ~T);
-	fn notifyObservers(&mut self); 
+trait Observable<'a, T: Observer> {
+	fn addObserver(&mut self, observer: &'a T);
+	fn deleteObserver(&mut self, observer: &'a T);
+	fn notifyObservers(&self); 
 }
-//
+
+// Define Observer and Observable
 struct Display {
 	name: ~str,
 }
-struct Weather<T> {
+struct Weather<'a, T> {
 	temperature: f64,
-	observers: ~[~T]
+	observers: Vec<&'a T>
 }
-impl Weather<Display> {
+impl<'a> Weather<'a, Display> {
 	fn setTemperature(&mut self, temperature: f64) {
 		self.temperature = temperature;
 		self.notifyObservers();
@@ -31,7 +45,7 @@ impl Weather<Display> {
  */
 //impl<U: Observable> Observer<U> for Display<U> {
 impl Observer for Display {
-	fn update(&mut self) {
+	fn update(&self) {
 		println!("Display {} updated!", self.name);
 	}
 }
@@ -45,22 +59,33 @@ impl std::cmp::Eq for Display {
 		self.name == other.name
 	}
 }
-impl<T: Observer+Eq> Observable<T> for Weather<T> {
-	fn addObserver(&mut self, observer: ~T) {
+impl std::fmt::Show for Display {
+	fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+		write!(f.buf, "Display {}", self.name)
+	}
+}
+impl<'a, T: Observer+Eq+std::fmt::Show> Observable<'a, T> for Weather<'a, T> {
+	fn addObserver(&mut self, observer: &'a T) {
+		println!("addObserver({});", observer);
 		self.observers.push(observer);
 	}
-	fn deleteObserver(&mut self, observer: ~T) {
+	fn deleteObserver(&mut self, observer: &'a T) {
 		let mut index = 0u;
-		for obs in self.observers.iter() {
-			if obs == &observer {
+		let mut found = false;
+		for &obs in self.observers.iter() {
+			if obs == observer {
+				println!("deleteObserver({});", observer);
+				found = true;
 				break;
 			}
 			index += 1;
 		}
-		self.observers.remove(index);
+		if found {
+			self.observers.remove(index);
+		}
 	}
-	fn notifyObservers(&mut self) {
-		for observer in self.observers.mut_iter() {
+	fn notifyObservers(&self) {
+		for &observer in self.observers.iter() {
 			observer.update();
 		}
 	}
@@ -69,12 +94,12 @@ impl<T: Observer+Eq> Observable<T> for Weather<T> {
 
 fn main() {
 	let display = Display::new(~"Desktop");
-	let mut weather = Weather{temperature: 19.0, observers: ~[]};
-	weather.addObserver(~display);
+	let mut weather = Weather{temperature: 19.0, observers: Vec::new()};
+	weather.addObserver(&display);
 	let display2 = Display::new(~"Desktop2");
-	weather.addObserver(~display2);
+	weather.addObserver(&display2);
 	weather.setTemperature(20.0);
-//	weather.deleteObserver(~display2);
+	weather.deleteObserver(&display2);
 	weather.setTemperature(21.0);
 }
 
